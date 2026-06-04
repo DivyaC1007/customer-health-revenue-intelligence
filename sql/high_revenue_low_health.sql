3b7e28b0fe80b160f89237d7daf4faaab114185c
DIVYA CHHABRA <divyachh1007@gmail.com>
2026-06-04 18:38:58 +0530

    Create high_revenue_low_health.sql

---
+++ sql/high_revenue_low_health.sql
@@ -0,0 +1,35 @@
+SELECT
+    a.customer_id,
+    a.company_name,
+    a.annual_revenue_usd,
+
+    (
+        (pu.feature_usage_score * 0.5)
+        +
+        (pu.monthly_logins * 0.1)
+        -
+        (st.ticket_count * 2)
+        +
+        CASE
+            WHEN r.renewal_status = 'Renewed' THEN 20
+            WHEN r.renewal_status = 'Pending' THEN 10
+            ELSE 0
+        END
+    ) AS customer_health_score
+
+FROM accounts a
+
+LEFT JOIN product_usage pu
+    ON a.customer_id = pu.customer_id
+
+LEFT JOIN support_tickets st
+    ON a.customer_id = st.customer_id
+
+LEFT JOIN renewals r
+    ON a.customer_id = r.customer_id
+
+ORDER BY
+    annual_revenue_usd DESC,
+    customer_health_score ASC
+
+LIMIT 20;
